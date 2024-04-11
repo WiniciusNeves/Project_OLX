b6df9805a2950dc6db567277ae55af904f4c3337
Winicius-Neves <winiciusneves2004@hotmail.com>
2024-04-11 17:10:10 -0300

    updade

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
 $con = mysqli_connect('127.0.0.1:3307', 'root', '');
-$database = mysqli_select_db($con, 'sistema_login');
+$database = mysqli_select_db($con, 'project_olx');
 
 /*if (!$con || !mysqli_select_db($con, 'sistema_login')) {
         die('Could not connect to database');
