bc770433caadc80d51e7fbf20d4720133816d02a
Winicius-Neves <winiciusneves2004@hotmail.com>
2024-04-10 23:26:32 -0300

    updade

--- src/index.php
+++ src/index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OLX Copy</title>
 </head>
+
 <body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
     <?php
-        include 'config.php'; 
-        
+    include 'config.php';
+
     ?>
-    
+
+
 </body>
+
 </html>
