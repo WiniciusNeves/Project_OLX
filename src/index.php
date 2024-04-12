681cf16f9a4b8c3e5f226166e2a6df422d52c8b8
Winicius Neves <127152668+WiniciusNeves@users.noreply.github.com>
2024-04-12 10:19:46 -0300

    Update and rename index.html to index.php

--- src/index.php
+++ src/index.php
@@ -10,8 +10,10 @@
 
 <body>
     <header>
-        <div class="container-header" style="border-bottom:1px solid black; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius : 0px 0px 10px 10px; height: 80px;">
-            <img src="./public/images/OLX-logo-big.png" alt="" width="50" height="50" style="margin: 10px 50px 0px 50px;">
+        <div class="container-header"
+            style="border-bottom:1px solid black; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius : 0px 0px 10px 10px; height: 80px;">
+            <img src="./public/images/OLX-logo-big.png" alt="" width="50" height="50"
+                style="margin: 10px 50px 0px 50px;">
             <a href="Anunciar.php"><input type="button" value="Anunciar"></a>
             <a href="login.php"><input type="button" value="Login" style="background-color: white; color: black"></a>
         </div>
@@ -20,19 +22,21 @@
         <div class="container-aside">
             <div class="categories">
                 <div class="box" id="box1">
-                    <a href="filter?category=1"><img src="./public/images/1.png" width="50" height="50" alt="" style="margin-left: 8px;"></a>
+                    <a href="#"><img src="./public/images/1.png" width="50" height="50" alt=""
+                            style="margin-left: 8px;"></a>
                     <ul style="list-style-type: none; padding: 0; margin: 0">
                         <li>tecnologia</li>
                     </ul>
                 </div>
                 <div class="box" id="box2">
-                    <a href="filter?category=2"><img src="./public/images/2.png" width="50" height="50" alt="" style="margin-left: 10px;"></a>
+                    <a href="#"><img src="./public/images/2.png" width="50" height="50" alt=""
+                            style="margin-left: 10px;"></a>
                     <ul style="list-style-type: none; padding: 0; margin: 0">
                         <li>decoração</li>
                     </ul>
                 </div>
                 <div class="box" id="box3">
-                    <a href="filter?category=3"><img src="./public/images/3.png" width="50" height="50" alt=""></a>
+                    <a href="#"><img src="./public/images/3.png" width="50" height="50" alt=""></a>
                     <ul style="list-style-type: none; padding: 0; margin: 0">
                         <li>Veiculos</li>
                     </ul>
@@ -42,20 +46,24 @@
     </aside>
     <main>
         <div class="container-main">
-            <span class="text">
+            <span
+                style="font-size: 20px ; font-weight: bold ; color: rebeccapurple; text-decoration: underline ; position: relative; top: 100px ; left: 80px">
+                Mais recentes
+                <hr style="position: fixed; left: 70px; border: 1px solid rebeccapurple; width: 25vw;" />
 
             </span>
-            <span class="text">
-                
-            </span>
+
+
+
 
         </div>
 
     </main>
-    <footer style="position: fixed; bottom: 0; width: 100vw; height: 30px; background-color: black; color: white; font-size: 8px; text-align: center; border-radius: 0px 0px 10px 10px" >
-    <h1>Direitos Reservados - 2024 por ||Winicius Neves || João Brasil || Vinicius Anacleto || Matheus Ziem ||</h1>
+    <footer
+        style="position: fixed; bottom: 0; width: 100vw; height: 30px; background-color: black; color: white; font-size: 8px; text-align: center; border-radius: 0px 0px 10px 10px">
+        <h1>Direitos Reservados - 2024 por ||Winicius Neves || João Brasil || Vinicius Anacleto || Matheus Ziem ||</h1>
     </footer>
-   
+
 
 
 </body>
